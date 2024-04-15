ee14d0ef31e417bd7475cf4288d6516cb10f012e
muhammad6153 <owais6153@gmail.com>
2024-04-15 07:52:18 +0500

    Prevent Fatal Error

--- index.php
+++ index.php
@@ -11,13 +11,35 @@ else
     echo '</pre>';
 }
 
+
 function fetchPage($link){
-    $pageCurl = curl_init($link);
-    curl_setopt($pageCurl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($pageCurl, CURLOPT_BINARYTRANSFER, true);
-    $pageContent = curl_exec($pageCurl);
-    curl_close($pageCurl);
+    $pageContent =false;
+    try {
+        $pageCurl = curl_init($link);
+        curl_setopt($pageCurl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($pageCurl, CURLOPT_BINARYTRANSFER, true);
+        $pageContent = curl_exec($pageCurl);
+        if($pageContent === false){
+            throw new Exception('Failed to fetch page: ' . curl_error($pageCurl));
+        }
+        curl_close($pageCurl);
+    } catch (Exception $e) {
+        echo 'Error fetching page';
+    }
     return $pageContent;
+}
+
+function downloadImage($imageUrl, $savePath){
+    try {
+        $imageData = file_get_contents($imageUrl);
+        if($imageData === false){
+            throw new Exception('Failed to download image from URL: ' . $imageUrl);
+        }
+        file_put_contents($savePath, $imageData);
+    } catch (Exception $e) {
+        // Handle the exception, you can log it or display an error message
+        echo 'Error downloading image';
+    }
 }
 
 
@@ -38,6 +60,7 @@ $FILE_URL_PREFIX = 'https://www.illinoistreasurer.gov';
 
 foreach($pages as $index => $page){
     $htmlString = fetchPage($page['scrap_from']);
+    if(!$htmlString) return;
     libxml_use_internal_errors(true);
     $doc = new DOMDocument();
     $doc->loadHTML($htmlString);
@@ -53,10 +76,9 @@ foreach($pages as $index => $page){
         else if($contentStructure['type'] === 'image'){
             $imageUrl = isset($contentElements[0]) ? $contentElements->item(0)->getAttribute('src') : '';
             if($imageUrl !== '' && $imageUrl !== '../#'){
-                $imageData = file_get_contents($FILE_URL_PREFIX . $imageUrl);
                 $filename = basename($imageUrl);
                 $savePath = $UPLOAD_TO . $filename; 
-                file_put_contents($savePath, $imageData);
+                downloadImage( $FILE_URL_PREFIX .  $imageUrl, $savePath);
                 $contentValue = $savePath;
             }            
         }
